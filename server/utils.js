7fd97816ac31c525c91765fb20bdc076aec697b5
Eva Raymond <evathecoderr@outlook.com>
2017-10-23 02:53:07 +0100

    database json helpers

--- server/utils.js
+++ server/utils.js
@@ -1,10 +1,10 @@
 var fs = require('fs')
 
-var students = JSON.parse(fs.readFileSync('../db/db.json')) //blocking read
+var students = JSON.parse(fs.readFileSync('./db/db.json')) //blocking read
 
 var rawData = JSON.stringify(students, null, 4)
 
-fs.writeFile('../db/db.json', rawData, callback)
+fs.writeFile('./db/db.json', rawData, callback)
 
 function callback(){
     console.log('Saved to database')
